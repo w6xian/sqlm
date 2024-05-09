8d501245ce80469d5d73c2133389484f361f3041
Leo <todayliu@hotmail.com>
2024-05-09 19:14:02 +0800

    Update tx.go

--- tx.go
+++ tx.go
@@ -1,5 +1,12 @@
 package sqlm
 
+import "database/sql"
+
+type ITable interface {
+	Table(tbl string) *Table
+	Exec(query string, args ...interface{}) (sql.Result, error)
+}
+
 type Tx struct {
 	db         *Db
 	connection TxConn
@@ -12,4 +19,8 @@ func (tx *Tx) Use(dbc TxConn) {
 func (tx *Tx) Table(tbl string) *Table {
 	svr := tx.db.server
 	return Tb(tbl).UseLog(tx.db.log).Use(tx.db).UseConn(tx.connection).PreTable(svr.Pretable)
+}
+
+func (tx *Tx) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return tx.db.conn.Exec(query, args...)
 }
